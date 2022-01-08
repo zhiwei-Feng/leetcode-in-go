688d48f87ca6caaac2d3f668c4723e9fcd3503e6
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-01-08 18:00:15 +0800

    append problem n0089

---
+++ src/n0089/solution_test.go
@@ -0,0 +1,27 @@
+package n0089
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_grayCode(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"case1", args{1}, []int{0,1}},
+		{"case2", args{4}, []int{0,1,3,2,6,7,5,4,12,13,15,14,10,11,9,8}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := grayCode(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("grayCode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
