a0d101878634d87d7abbfce2db42a7c5805b9931
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-01-09 22:00:18 +0800

    append problem offer0006

---
+++ src/offer0006/solution.go
@@ -0,0 +1,30 @@
+package offer0006
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reversePrint(head *ListNode) []int {
+	ans := []int{}
+	p := head
+	for p != nil {
+		ans = append(ans, p.Val)
+		p = p.Next
+	}
+	i, j := 0, len(ans)-1
+	for i < j {
+		ans[i], ans[j] = ans[j], ans[i]
+		i++
+		j--
+	}
+	return ans
+}
