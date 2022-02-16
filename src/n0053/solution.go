906a7857790493c8a19145f7b19a30e42da7e915
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-02-16 22:04:21 +0800

    update n0053

--- src/n0053/solution.go
+++ src/n0053/solution.go
@@ -1,18 +1,34 @@
 package n0053
 
 func maxSubArray(nums []int) int {
-	dp := make([]int, len(nums))
-	dp[0] = nums[0]
-	ans := dp[0]
-	for i := 1; i < len(nums); i++ {
-		dp[i] = max(nums[i], dp[i-1]+nums[i])
-		if dp[i] > ans {
-			ans = dp[i]
+	ans := 0
+	tmpMax := 0
+	for i, v := range nums {
+		if i == 0 {
+			tmpMax = v
+		} else {
+			tmpMax = max(nums[i], tmpMax+nums[i])
+		}
+		if tmpMax > ans {
+			ans = tmpMax
 		}
 	}
-
 	return ans
 }
+
+// func maxSubArray(nums []int) int {
+// 	dp := make([]int, len(nums))
+// 	dp[0] = nums[0]
+// 	ans := dp[0]
+// 	for i := 1; i < len(nums); i++ {
+// 		dp[i] = max(nums[i], dp[i-1]+nums[i])
+// 		if dp[i] > ans {
+// 			ans = dp[i]
+// 		}
+// 	}
+
+// 	return ans
+// }
 
 func max(i1, i2 int) int {
 	if i1 > i2 {
