9c51df9331a204053ad22abfb5872194f35d2291
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-02-18 23:26:58 +0800

    update n0198

--- src/n0198_house-robber/house-robber.go
+++ src/n0198_house-robber/house-robber.go
@@ -1,24 +1,21 @@
 package n0198
 
 func rob(nums []int) int {
-    if len(nums)<=2 {
-        return max(nums...)
-    }
-    dp := make([]int, len(nums))
-    dp[0]=nums[0]
-    dp[1]=max(nums[0],nums[1])
-    for i:=2;i<len(nums);i++{
-        dp[i]=max(dp[i-2]+nums[i], dp[i-1])
-    }
-    return dp[len(nums)-1]
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	prev2, prev1 := nums[0], max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		prev2, prev1 = prev1, max(prev1, prev2+nums[i])
+	}
+
+	return prev1
 }
 
-func max(x ...int) int {
-	maxN := x[0]
-	for i := 1; i < len(x); i++ {
-		if x[i] > maxN {
-			maxN = x[i]
-		}
+func max(i, j int) int {
+	if i > j {
+		return i
 	}
-	return maxN
+	return j
 }
