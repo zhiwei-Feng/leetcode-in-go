d859e36e4995d746ccdabbd2204d7a40c903b647
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-02-19 22:12:17 +0800

    update n0139

--- src/n0139_word-break/word-break.go
+++ src/n0139_word-break/word-break.go
@@ -1,6 +1,33 @@
 package n0139
 
 func wordBreak(s string, wordDict []string) bool {
+	dp := make([]bool, len(s)) // dp[i]表示[0..<=i]能否拆分
+	wordCount := make(map[string]bool)
+	for _, v:= range wordDict {
+		wordCount[v]=true
+	}
+	if _, ok := wordCount[string(s[0])];ok{
+		dp[0]=true
+	}
+	for i:=1;i<len(s);i++{
+		if wordCount[s[:i+1]]{
+			dp[i]=true
+			continue
+		}
+		for j:=0;j<i;j++{
+			last := s[j+1:i+1]
+			if dp[j] {
+				if _, ok:=wordCount[last];ok{
+					dp[i]=true
+					break
+				}
+			}
+		}
+	}
+	return dp[len(s)-1]
+}
+
+func wordBreak_(s string, wordDict []string) bool {
 	count := make(map[string]int)
 	minLen := 1<<7-1
 	maxLen := 0
