a2e7a841ce9f03c0df031bdd4cb0d4e3a56650fa
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-02-19 20:34:42 +0800

    update n0394

--- src/n0394_decode-string/solution.go
+++ src/n0394_decode-string/solution.go
@@ -6,52 +6,57 @@ import (
 )
 
 func decodeString(s string) string {
-	var deBuilder strings.Builder
-	for i := 0; i < len(s); i++ {
-		if s[i] >= 'a' && s[i] <= 'z' {
-			deBuilder.WriteByte(s[i])
-		} else if s[i] >= '0' && s[i] <= '9' {
-			// 0 is invalid
-			// find entire integer
-			number, stringStart := findNumber(i, s)
-			end, res := decode(stringStart, s, number)
-			deBuilder.WriteString(res)
-			i = end
+	ptr := 0
+	stack := []string{}
+	for ptr < len(s) {
+		if s[ptr] >= '1' && s[ptr] <= '9' {
+			// digit
+			repeatNum, nextStart := getDigit(s, ptr)
+			stack = append(stack, repeatNum)
+			ptr = nextStart
+		} else if (s[ptr] >= 'a' && s[ptr] <= 'z') || s[ptr] == '[' {
+			stack = append(stack, string(s[ptr]))
+			ptr++
+		} else {
+			// ']'
+			sub := []string{}
+			for stack[len(stack)-1] != "[" {
+				sub = append(sub, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			stack = stack[:len(stack)-1]
+			repeat, _ := strconv.Atoi(stack[len(stack)-1])
+			stack = stack[:len(stack)-1]
+			reverseArr(sub)
+			t := strings.Repeat(getString(sub), repeat)
+			stack = append(stack, t)
+			ptr++
 		}
 	}
-	return deBuilder.String()
+	return getString(stack)
 }
 
-func findNumber(st int, s string) (int, int) {
-	en := st
-	for ; en < len(s); en++ {
-		if s[en] >= '0' && s[en] <= '9' {
+func getDigit(s string, start int) (string, int) {
+	for i := start + 1; i < len(s); i++ {
+		if s[i] >= '0' && s[i] <= '9' {
 			continue
 		} else {
-			break
+			return s[start:i], i
 		}
 	}
-	number, _ := strconv.Atoi(s[st : en])
-	return number, en + 1
+	return string(s[start]), start + 1
 }
 
-func decode(st int, s string, num int) (int, string) {
-	var builder strings.Builder
-	for i := st; i < len(s); i++ {
-		if s[i] == ']' {
-			tmp := builder.String()
-			for j := 0; j < num-1; j++ {
-				builder.WriteString(tmp)
-			}
-			return i, builder.String()
-		} else if s[i] >= '0' && s[i] <= '9' {
-			number, stringStart := findNumber(i, s)
-			end, res := decode(stringStart, s, number)
-			builder.WriteString(res)
-			i = end
-		} else {
-			builder.WriteByte(s[i])
-		}
+func getString(stack []string) string {
+	var ans strings.Builder
+	for _, v := range stack {
+		ans.WriteString(v)
 	}
-	return -1, ""
+	return ans.String()
+}
+
+func reverseArr(arr []string) {
+	for i := 0; i < len(arr)/2; i++ {
+		arr[i], arr[len(arr)-1-i] = arr[len(arr)-1-i], arr[i]
+	}
 }
