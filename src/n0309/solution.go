2e7a7901a359c66b6d7e65edb903fad0832c8aa1
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-02-20 19:19:44 +0800

    update n0309

--- src/n0309/solution.go
+++ src/n0309/solution.go
@@ -9,7 +9,7 @@ func maxProfit(prices []int) int {
 	}
 	dp := make([][3]int, len(prices))
 	dp[0][0] = -prices[0]
-	dp[0][1] = -1
+	dp[0][1] = 0
 	dp[0][2] = 0
 	for i := 1; i < len(prices); i++ {
 		dp[i][0] = max(dp[i-1][0], dp[i-1][2]-prices[i]) // 当天持有股票
