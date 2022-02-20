78ebf46a682fe2d9a971878789e44a859fa36456
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-02-20 15:50:39 +0800

    update n0076

--- src/n0076/solution.go
+++ src/n0076/solution.go
@@ -1,6 +1,44 @@
 package n0076
 
 func minWindow(s string, t string) string {
+	ans := ""
+	winSt := 0
+	winMap := make(map[byte]int)
+	winMatch := 0
+	for i := range t {
+		winMap[t[i]]++
+	}
+
+	for winEnd := 0; winEnd < len(s); winEnd++ {
+		curByte := s[winEnd]
+		if _, ok := winMap[curByte]; ok {
+			winMap[curByte]--
+			if winMap[curByte] == 0 {
+				winMatch++
+			}
+		}
+		if winMatch < len(winMap) {
+			continue
+		}
+		for winMatch == len(winMap) {
+			// update ans
+			if ans == "" || (winEnd-winSt+1) < len(ans) {
+				ans = s[winSt : winEnd+1]
+			}
+			delByte := s[winSt]
+			if _, ok := winMap[delByte]; ok {
+				if winMap[delByte] == 0 {
+					winMatch--
+				}
+				winMap[delByte]++
+			}
+			winSt++
+		}
+	}
+	return ans
+}
+
+func minWindow_(s string, t string) string {
 	winSt := 0
 	wordMap := make(map[byte]int)
 	wordCount := 0
