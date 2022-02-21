d31cddca8ae6418aa2f663a1c08265d61a7c3d4f
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-02-21 19:06:58 +0800

    append n0236

---
+++ src/n0236/solution.go
@@ -0,0 +1,24 @@
+package n0236
+
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+    if root.Val == p.Val || root.Val == q.Val {
+        return root
+    }
+    left := lowestCommonAncestor(root.Left, p, q)
+    right := lowestCommonAncestor(root.Right, p, q)
+    if left != nil && right != nil {
+        return root
+    }
+    if left == nil {
+        return right
+    }
+    return left
+}
