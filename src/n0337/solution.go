9c3d7bb0ed4d175ace721919b1ebe4a1df8226d0
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-02-22 13:49:29 +0800

    update n0337

--- src/n0337/solution.go
+++ src/n0337/solution.go
@@ -6,24 +6,29 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+type SubTreeStatus struct {
+	Selected int
+	NotSelected int
+}
+
 // f(o)表示o被选中的情况下子树的最大权值
 // g(o)表示o不被选中的情况下子树的最大权值
 func rob(root *TreeNode) int {
-	f := make(map[*TreeNode]int)
-	g := make(map[*TreeNode]int)
-	dfs(root, f, g)
-	return max(f[root], g[root])
+	ans := dfs(root)
+	return max(ans.Selected, ans.NotSelected)
 }
 
-func dfs(root *TreeNode, f, g map[*TreeNode]int) {
+func dfs(root *TreeNode) SubTreeStatus {
 	if root == nil {
-		return
+		return SubTreeStatus{0,0}
 	}
 
-	dfs(root.Left, f, g)
-	dfs(root.Right, f, g)
-	f[root] = g[root.Left] + g[root.Right] + root.Val
-	g[root] = max(f[root.Left], g[root.Left]) + max(f[root.Right], g[root.Right])
+	sub := SubTreeStatus{}
+	l := dfs(root.Left)
+	r := dfs(root.Right)
+	sub.Selected = l.NotSelected+r.NotSelected+root.Val
+	sub.NotSelected = max(l.NotSelected, l.Selected) + max(r.NotSelected, r.Selected)
+	return sub
 }
 
 func max(x, y int) int {
@@ -32,3 +37,30 @@ func max(x, y int) int {
 	}
 	return y
 }
+
+// // f(o)表示o被选中的情况下子树的最大权值
+// // g(o)表示o不被选中的情况下子树的最大权值
+// func rob(root *TreeNode) int {
+// 	f := make(map[*TreeNode]int)
+// 	g := make(map[*TreeNode]int)
+// 	dfs(root, f, g)
+// 	return max(f[root], g[root])
+// }
+
+// func dfs(root *TreeNode, f, g map[*TreeNode]int) {
+// 	if root == nil {
+// 		return
+// 	}
+
+// 	dfs(root.Left, f, g)
+// 	dfs(root.Right, f, g)
+// 	f[root] = g[root.Left] + g[root.Right] + root.Val
+// 	g[root] = max(f[root.Left], g[root.Left]) + max(f[root.Right], g[root.Right])
+// }
+
+// func max(x, y int) int {
+// 	if x > y {
+// 		return x
+// 	}
+// 	return y
+// }
