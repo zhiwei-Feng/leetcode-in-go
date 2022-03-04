35963366d2457f9d39efeb04e7f6942313afa717
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-03-04 13:51:22 +0800

    update n0279

--- main.go
+++ main.go
@@ -2,67 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-type Node struct {
-	Val  int
-	Next *Node
-}
-
-func Solver(head *Node, k int) *Node {
-	// reverse
-	newHead := reverse(head)
-	dummy := &Node{}
-	prevH := dummy
-	H := newHead
-	p := head
-	count := 0
-	for p != nil {
-		count++
-		if count == k {
-			nextP := p.Next
-			p.Next = nil // 截断
-			newH := reverse(H)
-			prevH.Next = newH
-			H.Next = nextP
-			p = nextP
-			count = 0
-			prevH = H
-			H = H.Next
-		} else {
-			p = p.Next
-		}
-	}
-	finalHead := reverse(dummy.Next)
-	return finalHead
-}
-
-func reverse(head *Node) *Node {
-	p := &Node{Next: nil}
-	q := head
-	for q != nil {
-		nextq := q.Next
-		q.Next = p.Next
-		p.Next = q
-		q = nextq
-	}
-	return p.Next
-}
+var a int
 
 func main() {
-	head := &Node{}
-	tail := head
-	for i := 1; i <= 8; i++ {
-		tail.Next = &Node{Val: i}
-		tail = tail.Next
-	}
-	for p := head.Next; p != nil; p = p.Next {
-		fmt.Print(p.Val)
-	}
-	fmt.Println()
-	newHead := Solver(head.Next, 3)
-	for p := newHead; p != nil; p = p.Next {
-		fmt.Print(p.Val)
-	}
-	fmt.Println()
+	go func() {
+		a = 1
+	}()
+	go func() {
+		a = 2
+	}()
+	time.Sleep(time.Second)
+	fmt.Println(a)
 }
--- src/n0279/solution.go
+++ src/n0279/solution.go
@@ -3,13 +3,44 @@ package n0279
 import "math"
 
 func numSquares(n int) int {
+	coins := make([]int, 0)
+	for j := 1; j*j <= n; j++ {
+		coins = append(coins, j*j)
+	}
+	f := make([][]int, len(coins))
+	for i := 0; i < len(coins); i++ {
+		f[i] = make([]int, n+1)
+		for j := 0; j <= n; j++ {
+			f[i][j] = math.MaxInt32
+		}
+	}
+	for i := 0; i < len(coins); i++ {
+		f[i][0] = 0
+	}
+	for j := 1; j <= n; j++ {
+		if j == coins[0] {
+			f[0][j] = 1
+		}
+	}
+	for i := 1; i < len(coins); i++ {
+		for j := 0; j <= n; j++ {
+			f[i][j] = f[i-1][j]
+			if j >= coins[i] {
+				f[i][j] = min(f[i][j], f[i][j-coins[i]]+1)
+			}
+		}
+	}
+	return f[len(coins)-1][n]
+}
+
+func numSquares_1d(n int) int {
 	f := make([]int, n+1)
 	for i := 1; i <= n; i++ {
 		minn := math.MaxInt32
-		for j:=1;j*j<=i;j++ {
+		for j := 1; j*j <= i; j++ {
 			minn = min(minn, f[i-j*j]+1)
 		}
-		f[i]=minn
+		f[i] = minn
 	}
 	if f[n] == math.MaxInt16 {
 		return -1
