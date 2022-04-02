4e3d473ee941f74060a9b5cd01b801dbc998cb85
ᴊᴏᴇ <zhiwei.feng1995@gmail.com>
2022-04-02 14:30:29 +0800

    append n0468

---
+++ src/n0468/solution.go
@@ -0,0 +1,61 @@
+package n0468
+
+import (
+	"strconv"
+	"strings"
+)
+
+func validIPAddress(queryIP string) string {
+	if checkIPv4(queryIP) {
+		return "IPv4"
+	} else if checkIPv6(queryIP) {
+		return "IPv6"
+	} else {
+		return "Neither"
+	}
+}
+
+func checkIPv4(query string) bool {
+	ips := strings.Split(query, ".")
+	if len(ips) != 4 {
+		return false
+	}
+	for _, ip := range ips {
+		intVal, err := strconv.Atoi(ip)
+		if err != nil || intVal > 255 || intVal < 0 {
+			return false
+		}
+		if intVal == 0 {
+			continue
+		}
+		count := 0
+		for intVal > 0 {
+			count++
+			intVal /= 10
+		}
+		if count != len(ip) {
+			return false
+		}
+	}
+	return true
+}
+
+func checkIPv6(query string) bool {
+	ips := strings.Split(query, ":")
+	if len(ips) != 8 {
+		return false
+	}
+	for _, ip := range ips {
+		if len(ip) > 4 || len(ip) < 1 {
+			return false
+		}
+		for i := range ip {
+			if (ip[i] >= '0' && ip[i] <= '9') || (ip[i] >= 'a' && ip[i] <= 'z') || (ip[i] >= 'A' && ip[i] <= 'Z') {
+				continue
+			} else {
+				return false
+			}
+		}
+	}
+	return true
+}
